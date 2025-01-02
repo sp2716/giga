fb21ced686de5ad303cff951db559414acec0e2f
Sean Palastro <Sean.Palastro@sensiaglobal.com>
2025-01-02 16:11:09 -0500

    added threadx, display libs

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,3 +1,4 @@
+
 #include <Wire.h>
 #include <Ethernet.h>
 #include <ArduinoGraphics.h>
@@ -13,34 +14,44 @@
 #include <lv_conf_9.h>
 #include <st7701.h>
 #include <video_modes.h>
+#include <vector>
 #include "version.h"
 
 //Globals are Serial, Serial1, Serial2, Serial3 for the 4 UARTS in the MEGA R1
 
+//Initialize Giga Display Shield with global application interface
 Arduino_H7_Video Display(800, 480, GigaDisplayShield);
 
+//buffer for serial output messages
+char buf[100];
+
 void setup() {
-  Serial.begin(115200);
   // put your setup code here, to run once:
-  while (!Serial) {
-    ;  //wait for UART Success
-  }
-  Serial.println("Starting Setup");
-  char buf[100];
-  sprintf(buf, "Setup of Main Application Version %.2i.%.2i.%.2i", APP_SW_PART, APP_REV_MAJOR,APP_REV_MINOR);
+  //Initialize UART0
+  Serial.begin(115200);
+  while (!Serial) { ; }
+  Serial.println("UART0 Initialized at 115200 baud");
+  //intialize I2C interface for sensors
+  //Wire.begin();
+
+  sprintf(buf, "Software P\N %.2i Revision %.2i.%.2i",
+          APP_SW_PART, APP_REV_MAJOR, APP_REV_MINOR);
   Serial.println(buf);
+  //iniatizlie display output
   Display.clear();
   Display.beginDraw();
   Display.stroke(255, 255, 255);
   Display.text(buf, 50, 50);
   Display.endDraw();
+
+  Serial.println("Initialization Complete - Starting Application");
 }
+
 void loop() {
- 
+  sprintf(buf, "App loop starting at memory address %p", (void*)loop);
+  Serial.println(buf);
   // put your main code here, to run repeatedly:
-   Serial.println("Starting Main Loop");
-   while(1){
-    //trap
-    ;
-   }
+  while (1) {
+    delayMicroseconds(100 * 1000);  //100ms delay
+  }
 }
--- sketch/version.h
+++ sketch/version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define APP_SW_PART 1
+#define APP_SW_PART 100
 #define APP_REV_MAJOR 1
 #define APP_REV_MINOR 1
 
