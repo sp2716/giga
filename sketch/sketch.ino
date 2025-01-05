cbf541ec60beb0da1b98e84c9651ac8fdcafceff
Sean Palastro <Sean.Palastro@sensiaglobal.com>
2025-01-05 18:41:53 -0500

    working touchscreen writes basic text, and serial port (USB UART0) printing via 115200 to pcomm terminal emulator for debugging purposes

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,34 +1,12 @@
-#include <Arduino_Threads.h>
-
-//ardiuno libs
-#include <Wire.h>
-
-//custom
-#include "version.h"
-#include "worker.h"
-
-void dispHandler(void);
-
+#include <Arduino.h>
 void setup() {
+
   Serial.begin(9600);
-  while (!Serial) {}
-  rtos::Thread* t = new rtos::Thread(osPriorityNormal, OS_STACK_SIZE, nullptr, "dispThrd");
-  t->start(dispHandler);
+  //while(!Serial){}
+  Serial.println("Hello Sean");
 }
 
 void loop() {
+  Serial.println("Working");
   
-}
-
-void dispHandler(void) {
-  Serial.begin(9600);
-  while (!Serial) {}
-  for (;;) {
-    //touchscreen events
-    // should be handled with interrupts instead - this is a processing routine.
-    //100 ms sleep
-    rtos::Thread::sleep_for(rtos::Kernel::Clock::duration_u32(100));
-    //do something every 100ms
-    Serial.println("Display Thread doing something");
-  }
 }
