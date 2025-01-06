7d83470fd43468e07d938b230e4769949b5d8024
Sean Palastro <seanpala@gmail.com>
2025-01-05 22:20:00 -0500

    great example of working wifi connection and nicer font with horizontal rotation

---
+++ sketch/gfx_demo/ourtypes.h
@@ -0,0 +1,12 @@
+#ifndef OURTYPES_H
+#define OURTYPES_H
+
+#define RED 0xf800
+#define BLUE 0x001F
+#define GREEN 0x07E0
+#define WHITE 0xFFFF
+#define BLACK 0x0000
+
+
+
+#endif  //OURTYPES_H
--- sketch/worker.h
+++
@@ -1,6 +0,0 @@
-#ifndef WORKER_H
-#define WORKER_H
-#include <Arduino_GigaDisplayTouch.h>
-extern Arduino_GigaDisplayTouch * pTouchDetector;
-void* worker(void* vargs);
-#endif
